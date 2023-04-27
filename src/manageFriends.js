60148b21ae7376e9220502cf5e3ac0a54636b512
Walker Le <walker.le88@gmail.com>
2023-04-27 12:22:51 -0600

    lab complete

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
+let state = { friends: [] }
+let action = { type: 'friends/add' }
+
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type) {
+    case 'friends/add':
+      return {
+        friends: [...state.friends, action.payload]
+      };
+    case 'friends/remove':
+      return {
+        friends: state.friends.filter(friend => friend.id !== action.payload)
+      }
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,14 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+
+  switch(action.type) {
+    case 'presents/increase':
+      console.log('presents/increase action');
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      console.log('no action invoked')
+      return state;
+    }
+
 }
